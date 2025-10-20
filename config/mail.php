b63de18b9103312259af353d0e1af08a1759c622
azam <azoz20113040@gmail.com>
2025-10-20 08:01:14 +0300

    2025/10/20

--- config/mail.php
+++ config/mail.php
@@ -1,12 +1,38 @@
 <?php
+
 return [
 
-    'default' => env('MAIL_MAILER', 'sendgrid'),
+    /*
+    |--------------------------------------------------------------------------
+    | Default Mailer
+    |--------------------------------------------------------------------------
+    |
+    | البريد الافتراضي الذي سيتم استخدامه لإرسال جميع الرسائل.
+    |
+    */
+
+    'default' => env('MAIL_MAILER', 'smtp'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mailer Configurations
+    |--------------------------------------------------------------------------
+    |
+    | تكوين طرق الإرسال، هنا استخدمنا SMTP Gmail.
+    |
+    */
 
     'mailers' => [
-        'sendgrid' => [
-            'transport' => 'sendgrid',
-            'api_key' => env('SENDGRID_API_KEY'),
+
+        'smtp' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
         'log' => [
@@ -21,12 +47,22 @@ return [
         'failover' => [
             'transport' => 'failover',
             'mailers' => [
-                'sendgrid',
+                'smtp',
                 'log',
             ],
             'retry_after' => 60,
         ],
+
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Global "From" Address
+    |--------------------------------------------------------------------------
+    |
+    | البريد الافتراضي والاسم الذي سيتم إرسال الرسائل منه.
+    |
+    */
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
